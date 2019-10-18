303b00a82163442435c774aa0f824fd9866f5002
Eli Smaga <eli@confluent.io>
2019-10-18 16:45:11 -0700

    wip

--- zookeeper/test/test_zookeeper.py
+++ zookeeper/test/test_zookeeper.py
@@ -35,9 +35,10 @@ class ConfigTest(unittest.TestCase):
 
         # Copy SSL files.
         utils.run_cmd("mkdir -p {}".format(LOCAL_SECRETS_DIR))
-        print(utils.run_cmd("cp -rv {} {}".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
-        print(utils.run_cmd("cp -Rv {} {}".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
-        print(utils.run_cmd("cp {}config_jaas.conf {}config_jaas.conf".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
+        # print(utils.run_cmd("cp -rv {} {}".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
+        # print(utils.run_cmd("cp -Rv {} {}".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
+        # print(utils.run_cmd("cp {}config_jaas.conf {}config_jaas.conf".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
+        print(utils.run_cmd("cp -v {}*.conf {}".format(SECRETS_DIR, LOCAL_SECRETS_DIR)))
         print(utils.run_cmd("ls -l {}".format(LOCAL_SECRETS_DIR)))
         print(LOCAL_SECRETS_DIR)
         print(SECRETS_DIR)
