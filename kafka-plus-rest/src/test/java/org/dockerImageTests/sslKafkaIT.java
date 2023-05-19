d3628abbe5dd437d418bde08fd782bf9331beb1a
Ujjwal <ukalia@confluent.io>
2023-05-19 23:03:17 +0530

    fixes related to ub and encryption format

--- kafka-plus-rest/src/test/java/org/dockerImageTests/sslKafkaIT.java
+++ kafka-plus-rest/src/test/java/org/dockerImageTests/sslKafkaIT.java
@@ -50,7 +50,7 @@ public class sslKafkaIT {
 
     public GenericContainer container1;;
     @BeforeAll
-    public void setup() {
+    public void setup() throws InterruptedException {
         Yaml yaml = new Yaml();
         InputStream inputStream = getClass().getResourceAsStream("/sslconfigs.yml");
         Map<String,String> env = yaml.load(inputStream);
@@ -65,14 +65,15 @@ public class sslKafkaIT {
         catch(Exception  e) {
             System.out.println(container1.getLogs());
         }
+        Thread.sleep(3600000);
         String baseUrl = String.format("https://%s:%s",container1.getHost(),container1.getMappedPort(KAFKA_REST_PORT));
         String bootstrapUrl = String.format("%s:%s",container1.getHost(),container1.getMappedPort(KAFKA_PORT));
         Properties props = new Properties();
         props.put("ssl.keystore.type", "PKCS12");
         props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, "SSL");
-        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,getClass().getResource("/client-creds/kafka.client.truststore.pkcs121").getPath());
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,getClass().getResource("/client-creds/kafka.client.truststore.pkcs12").getPath());
         props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "confluent");
-        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, getClass().getResource("/client-creds/kafka.client.keystore.pkcs121").getPath());
+        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, getClass().getResource("/client-creds/kafka.client.keystore.pkcs12").getPath());
         props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "confluent");
         admin =  new Admin(bootstrapUrl,baseUrl,props,true);
         consumer = new Consumer(bootstrapUrl,"test-1",baseUrl,props,true);
@@ -81,7 +82,6 @@ public class sslKafkaIT {
     @AfterAll
     public void teardown(){
         System.out.println("tearing down");
-        System.out.println(container1.getLogs());
         container1.stop();
         producer.close();
     }
